de559f1cb788960483af9607f60b284fd81b3953
AzureAD\NiklasLindblad <niklas.o.lindblad@gmail.com>
2021-09-15 08:20:22 +0200

    Implemented T-rex

--- AnimalShop/AnimalShop/Tyrannosaurus.cs
+++ AnimalShop/AnimalShop/Tyrannosaurus.cs
@@ -24,5 +24,40 @@ namespace AnimalShop
             get;
             set;
         }
+
+        public Tyrannosaurus(int goatsPerDay, int teethCount, string skinColor, int age, double weight, float cost) : base(age, weight, cost)
+        {
+            GoatsPerDay = goatsPerDay;
+            TeethCount = teethCount;
+            SkinColor = skinColor;
+        }
+
+        // You can use different constructors in order to set values based on different criterias.
+        // In this case I base the feeding requirement based on the age and then using the above constructor to set the values
+        public Tyrannosaurus(int teethCount, string skinColor, int age, double weight, float cost)
+            : this(AverageGoatsPerDay(age), teethCount, skinColor, age, weight, cost)
+        {
+
+        }
+
+        public Tyrannosaurus() : this(4,60,"Green",1,4500,10000)
+        {
+
+        }
+
+        private static int AverageGoatsPerDay(int age)
+        {
+            switch(age)
+            {
+                case 1:
+                    return 4;
+                case 2:
+                    return 5;
+                case 3:
+                    return 6;
+                default:
+                    return 10;
+            }
+        }
     }
 }
