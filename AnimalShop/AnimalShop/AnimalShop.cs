eee1336f0e5305a9c39ce4ce156a33f935c813a6
AzureAD\NiklasLindblad <niklas.o.lindblad@gmail.com>
2021-09-15 09:08:30 +0200

    Created the AnimalShop logic

--- AnimalShop/AnimalShop/AnimalShop.cs
+++ AnimalShop/AnimalShop/AnimalShop.cs
@@ -15,9 +15,30 @@ namespace AnimalShop
             }
         }
 
-        public Animal SellAnimal()
+        public Animal SellAnimal(string animalName)
         {
-            throw new System.NotImplementedException();
+            Animal animalToSell;
+            switch(animalName.ToLower().Trim())
+            {
+                case "tyrannosaurus":
+                    animalToSell = new Tyrannosaurus();
+                    Console.WriteLine("You probably want to buy a few days worth of goats!");
+                    break;
+                case "stegosaurus":
+                    animalToSell = new Stegosaurus();
+                    break;
+                case "pterodactyl":
+                    animalToSell = new Pterodactyl();
+                    break;
+                case "goat":
+                    animalToSell = new Goat();
+                    break;
+                default:
+                    animalToSell = null;
+                    break;
+            }
+            Balance += animalToSell.Cost;
+            return animalToSell;
         }
     }
 }
