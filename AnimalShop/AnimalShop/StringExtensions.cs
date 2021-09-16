2f4b121aa8c4edaf30bdd02ce033df1af6b474bd
AzureAD\NiklasLindblad <niklas.o.lindblad@gmail.com>
2021-09-16 14:28:26 +0200

    Refactor StringExtension to be more robust

--- AnimalShop/AnimalShop/StringExtensions.cs
+++ AnimalShop/AnimalShop/StringExtensions.cs
@@ -1,9 +1,19 @@
-﻿using System.Threading;
-
-namespace AnimalShop
+﻿namespace AnimalShop
 {
     public static class StringExtensions
     {
-        public static string GetProper(this string text) => Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(text);
+        public static string GetProper(this string text)
+        {
+            if(text.Length == 0)
+            {
+                return text;
+            }
+            if(text.Length == 1)
+            {
+                return text.ToUpper();
+            }
+
+            return char.ToUpper(text[0]) + text[1..].ToLower();
+        }
     }
 }
