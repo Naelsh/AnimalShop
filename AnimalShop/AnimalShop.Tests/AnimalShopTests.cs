5584833b9ca80f5b59d3b500b9955dd60b167781
AzureAD\NiklasLindblad <niklas.o.lindblad@gmail.com>
2021-09-16 14:22:13 +0200

    Recreated project and created my first test!

---
+++ AnimalShop/AnimalShop.Tests/AnimalShopTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AnimalShop.Tests
+{
+    public class AnimalShopTests
+    {
+        [Fact]
+        public void SellAnimal_EmptyString()
+        {
+            // Arrange
+            Animal expected = null;
+            AnimalShop animalShop = new AnimalShop();
+
+            // Act
+            Animal actual = animalShop.SellAnimal("");
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
